1da493921f1dd766d0678abe33136f5d1e4bfe6b
manan <daxini.manan@gmail.com>
2024-06-08 00:48:32 +0530

    almost done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import bellicon from "../../assets/bell_icon.svg";
 import prof from "../../assets/profile_img.png";
 import dropdown from "../../assets/caret_icon.svg";
 import "./Navbar.css"; // Import your CSS file for Navbar styles
+import { logout } from "../../firebase";
+
 
 const Navbar = () => {
   const navRef = useRef();
@@ -29,22 +31,23 @@ const Navbar = () => {
   return (
     <div
       ref={navRef}
-      className={`Navbar w-full px-20 py-3 text-sm text-[#e5e5e5] z-10 flex justify-between ${
+      className={`Navbar w-full px-20 py-6 text-sm text-[#e5e5e5] bg-gradient-to-b from-[rgba(0,0,0,0.7)] to-transparent z-10 flex justify-between ${
         isScrolled ? "nav-dark fixed top-0 left-0" : ""
       }`}
     >
-      <img src={logo} alt="Logo" className="w-[96px] cursor-pointer" />
+      <div className="Navbar-left flex items-center gap-[50px]">
+        <img src={logo} alt="Logo" className="w-[96px]" />
+        <ul className="text-white flex list-none gap-5">
+          <li className="cursor-pointer">Home</li>
+          <li className="cursor-pointer">TV Shows</li>
+          <li className="cursor-pointer">Movies</li>
+          <li className="cursor-pointer">New & Popular</li>
+          <li className="cursor-pointer">My List</li>
+          <li className="cursor-pointer">Browse By Language</li>
+        </ul>
+      </div>
 
-      <ul className={`Navbar-menu flex list-none gap-5 ${isScrolled ? "text-white" : "text-black"}`}>
-        <li className="cursor-pointer">Home</li>
-        <li className="cursor-pointer">TV Shows</li>
-        <li className="cursor-pointer">Movies</li>
-        <li className="cursor-pointer">New & Popular</li>
-        <li className="cursor-pointer">My List</li>
-        <li className="cursor-pointer">Browse By Language</li>
-      </ul>
-
-      <div className="Navbar-icons flex gap-5 items-center ">
+      <div className="Navbar-right flex gap-5 items-center ">
         <img src={searchicon} alt="Search" className="w-[20px] cursor-pointer" />
         <p>Children</p>
         <img src={bellicon} alt="Notifications" />
@@ -53,7 +56,7 @@ const Navbar = () => {
           <img src={dropdown} alt="Dropdown" />
 
           <div className="dropdown absolute top-full right-0 w-max py-[18px] px-[22px] rounded-sm underline z-10 hidden group-hover:block bg-white text-black">
-            <p className="text-sm cursor-pointer">Sign Out of Netflix</p>
+            <p className="text-sm cursor-pointer" onClick={logout}>Sign Out of Netflix</p>
           </div>
         </div>
       </div>
--- src/Components/Pages/Login/Login.jsx
+++ src/Components/Pages/Login/Login.jsx
@@ -71,4 +71,3 @@ const Login = () => {
 };
 
 export default Login;
-
